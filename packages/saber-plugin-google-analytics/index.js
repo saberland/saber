1f0528fa99313f7c24f44ce91851171443a8adac
EGOIST <0x142857@gmail.com>
2019-01-21 13:09:31 +0800

    fix(google-analytics): properly set ID

--- packages/saber-plugin-google-analytics/index.js
+++ packages/saber-plugin-google-analytics/index.js
@@ -1,9 +1,11 @@
 const path = require('upath')
 
-exports.name = 'google-analytics'
+const ID = 'google-analytics'
+
+exports.name = ID
 
 exports.apply = (api, { trackId = false } = {}) => {
-  api.hooks.chainWebpack.tap('plugin-google-analytics', config => {
+  api.hooks.chainWebpack.tap(ID, config => {
     config.plugin('constants').tap(([options]) => [
       Object.assign(options, {
         __GA_TRACK_ID__: JSON.stringify(trackId)
