5de5825ed22c7cd2b8ac6f1d6d7f503f5b9dc464
EGOIST <0x142857@gmail.com>
2019-02-12 16:23:49 +0800

    refactor: ts-check resolvePackage.js

--- packages/saber/lib/utils/resolvePackage.js
+++ packages/saber/lib/utils/resolvePackage.js
@@ -1,8 +1,15 @@
+// @ts-check
 const path = require('path')
 const resolveFrom = require('resolve-from')
 
 const LOCAL_PATH_RE = /^[./]|(^[a-zA-Z]:)/
 
+/**
+ * @param {string} input
+ * @param {Object} options
+ * @param {string} [options.cwd=process.cwd()]
+ * @param {string=} options.prefix
+ */
 module.exports = (input, { cwd = process.cwd(), prefix } = {}) => {
   if (LOCAL_PATH_RE.test(input)) {
     return path.resolve(cwd, input)
