4ce604dc94d538076b36e2785160b94ccb4b821c
EGOIST <0x142857@gmail.com>
2019-02-13 17:30:55 +0800

    chore: tweak 404 page

--- packages/saber/lib/renderer/app/404.vue
+++ packages/saber/lib/renderer/app/404.vue
@@ -1,26 +1,29 @@
 <script>
+const NotFound = {
+  head: {
+    title: 'Not Found'
+  },
+  render(h) {
+    return h('div', { style: { margin: '2rem' } }, [
+      h('h1', { style: { margin: '0 0 20px 0' } }, ['Not Found']),
+      h('saber-link', {
+        props: {
+          to: '/'
+        },
+        style: {
+          fontSize: '16px'
+        }
+      }, ['← Back Home'])
+    ])
+  }
+}
+
 export default {
   functional: true,
   inject: ['layouts'],
   render(h, { injections: { layouts } }) {
-    if (layouts['404']) {
-      return h(layouts['404'])
-    }
-    return h('div', { class: 'not-found' }, [
-      h('h1', {}, ['Not Found']),
-      h('saber-link', {
-        props: {
-          to: '/'
-        }
-      }, ['Back Home'])
-    ])
+    return h(layouts['404'] || NotFound)
   }
 }
 </script>
-
-<style scoped>
-.not-found {
-  margin: 2rem;
-}
-</style>
 
