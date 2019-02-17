b501a0c097b50ae15eee62dab9d2a126552cdfd6
EGOIST <0x142857@gmail.com>
2019-02-17 21:19:21 +0800

    fix(markdown): ensure link href

--- packages/saber/lib/markdown/link-plugin.js
+++ packages/saber/lib/markdown/link-plugin.js
@@ -2,19 +2,22 @@ module.exports = function(md) {
   // eslint-disable-next-line camelcase
   md.renderer.rules.link_open = (tokens, idx, options, env, self) => {
     const token = tokens[idx]
+
     const hrefIndex = token.attrIndex('href')
     const link = token.attrs[hrefIndex]
 
-    if (link[1].startsWith('#')) {
-      token.tag = 'saber-link'
-      link[0] = ':to'
-      link[1] = `{hash: ${JSON.stringify(link[1].slice(1))}}`
-    } else if (link[1].startsWith('/')) {
-      token.tag = 'saber-link'
-      link[0] = 'to'
-    } else if (/^https?:\/\//.test(link[1])) {
-      token.attrSet('target', '_blank')
-      token.attrSet('rel', 'noopener noreferrer')
+    if (link) {
+      if (link[1].startsWith('#')) {
+        token.tag = 'saber-link'
+        link[0] = ':to'
+        link[1] = `{hash: ${JSON.stringify(link[1].slice(1))}}`
+      } else if (link[1].startsWith('/')) {
+        token.tag = 'saber-link'
+        link[0] = 'to'
+      } else if (/^https?:\/\//.test(link[1])) {
+        token.attrSet('target', '_blank')
+        token.attrSet('rel', 'noopener noreferrer')
+      }
     }
 
     return self.renderToken(tokens, idx, options)
