8480b748128ff7c271db9954d17f06cdd6a6d992
EGOIST <0x142857@gmail.com>
2019-04-12 21:43:22 +0800

    fix: simplify the default layout

--- packages/saber/vue-renderer/app/theme/layouts/default.vue
+++ packages/saber/vue-renderer/app/theme/layouts/default.vue
@@ -1,37 +1,12 @@
 <template>
   <div>
-    <h2 :class="$style.Title" v-if="page.attributes.title">{{ page.attributes.title }}</h2>
-    <div :class="$style.Content">
-      <slot name="default"></slot>
-    </div>
-    <div class="posts" v-if="page.posts && page.posts.length > 0">
-      <div class="post" v-for="post in page.posts" :key="post.attributes.permalink">
-        <h2 class="post-title">
-          <saber-link :to="post.attributes.permalink">{{ post.attributes.title }}</saber-link>
-        </h2>
-      </div>
-    </div>
+    <h2 v-if="page.attributes.title">{{ page.attributes.title }}</h2>
+    <slot name="default"></slot>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'LayoutDefault',
-  props: ['page'],
-  head() {
-    return {
-      title: this.page.attributes.title
-    }
-  }
+  props: ['page']
 }
 </script>
-
-<style module>
-.Title {
-  font-size: 3rem;
-}
-
-.Content {
-
-}
-</style>
