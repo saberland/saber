3bfca98442113278e6e4f8bc3eb61299348115ca
krmax44 <hi@krmax44.de>
2019-04-27 18:29:34 +0200

    feat: use corresponding leave transition

--- packages/saber/vue-renderer/app/helpers/set-transition.js
+++ packages/saber/vue-renderer/app/helpers/set-transition.js
@@ -30,13 +30,28 @@ export default ({ router }) => {
         }
       }
 
+      let transition = routeTransition || layoutTransition || {}
+      const leaveProperties = ['beforeLeave','leave', 'afterLeave', 'leaveClass', 'leaveCancelled', 'leaveToClass', 'leaveActiveClass']
+
+      for (const property of leaveProperties) {
+        const next = `nextTransition_${property}`
+        transition[next] = transition[property] // use current leave property for next route transition
+        
+        if (router.app.transition) {
+          transition[property] = router.app.transition[next] // apply the current route's leave properties
+        }
+        else {
+          transition[property] = undefined
+        }
+      }
+
       router.app.setTransition(
         Object.assign(
           {
             name: 'page',
             mode: 'out-in'
           },
-          routeTransition || layoutTransition
+          transition
         )
       )
       next()
