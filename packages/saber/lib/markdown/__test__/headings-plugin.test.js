8686b2de4fafaedfefe0da59fd3174c458d28286
EGOIST <0x142857@gmail.com>
2019-05-07 22:14:03 +0800

    fix test

--- packages/saber/lib/markdown/__test__/headings-plugin.test.js
+++ packages/saber/lib/markdown/__test__/headings-plugin.test.js
@@ -13,49 +13,45 @@ const input = `
 
 ##### Still in there`
 
-test('inject markdown headings enabled', () => {
+test('inject markdown headings enabled by default', () => {
   const md = new Markdown()
   const { env } = createEnv()
-  env.setAttribute('injectMarkdownHeadings', true)
   md.use(headingsPlugin)
   md.render(input, env)
-  expect(env.getAttribute('markdownHeadings')).toEqual(
-    expect.arrayContaining([
-      {
-        text: 'Heading',
-        slug: 'heading',
-        level: 1
-      },
-      {
-        text: 'Heading',
-        slug: 'heading-2',
-        level: 2
-      },
-      {
-        text: 'This time around, a link is present',
-        slug: 'this-time-around-a-link-is-present',
-        level: 3
-      },
-      {
-        text: 'Deep on so many levels!',
-        slug: 'deep-on-so-many-levels',
-        level: 4
-      },
-      {
-        text: 'Still in there',
-        slug: 'still-in-there',
-        level: 5
-      }
-    ])
-  )
+  expect(env.getAttribute('markdownHeadings')).toEqual([
+    {
+      text: 'Heading',
+      slug: 'heading',
+      level: 1
+    },
+    {
+      text: 'Heading',
+      slug: 'heading-2',
+      level: 2
+    },
+    {
+      text: 'This time around, a link is present',
+      slug: 'this-time-around-a-link-is-present',
+      level: 3
+    },
+    {
+      text: 'Deep on so many levels!',
+      slug: 'deep-on-so-many-levels',
+      level: 4
+    },
+    {
+      text: 'Still in there',
+      slug: 'still-in-there',
+      level: 5
+    }
+  ])
 })
 
 test('inject markdown headings disabled', () => {
   const md = new Markdown()
   const { env } = createEnv()
+  env.setAttribute('injectMarkdownHeadings', false)
   md.use(headingsPlugin)
   md.render(input, env)
-  expect(env.getAttribute('markdownHeadings')).toEqual(
-    expect.arrayContaining([])
-  )
+  expect(env.getAttribute('markdownHeadings')).toEqual([])
 })
