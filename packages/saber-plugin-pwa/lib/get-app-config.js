8acd29eaed3f926ee46d77dfcb619f5c829097f0
EGOIST <0x142857@gmail.com>
2019-05-07 17:27:05 +0800

    update html meta tags

---
+++ packages/saber-plugin-pwa/lib/get-app-config.js
@@ -0,0 +1,21 @@
+const defaults = {
+  name: 'Saber PWA App',
+  themeColor: '#3880ff', // saber.land theme color
+  msTileColor: '#000000',
+  appleMobileWebAppCapable: 'no',
+  appleMobileWebAppStatusBarStyle: 'default',
+  assetsVersion: '',
+  iconPaths: {
+    favicon16: 'img/icons/favicon-16x16.png',
+    favicon32: 'img/icons/favicon-32x32.png',
+    appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+    safariMaskIcon: 'img/icons/safari-mask-icon.svg',
+    msTileImage: 'img/icons/msapplication-icon-144x144.png'
+  }
+}
+
+module.exports = (config = {}) => {
+  return Object.assign({}, defaults, config, {
+    iconPaths: Object.assign({}, defaults.iconPaths, config.iconPaths)
+  })
+}
--- packages/saber-plugin-pwa/lib/index.js
+++ packages/saber-plugin-pwa/lib/index.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const generateManifest = require('./generate-manifest')
+const getAppConfig = require('./get-app-config')
+const createElement = require('./create-element')
 
 const ID = 'pwa'
 
@@ -7,7 +9,7 @@ exports.name = ID
 
 exports.apply = (
   api,
-  { notifyUpdates = true, generateSWOptions = {}, ...manifestOptions } = {}
+  { notifyUpdates = true, generateSWOptions = {}, ...appConfig } = {}
 ) => {
   if (api.dev) {
     // Uninstall server-worker.js in dev mode
@@ -27,6 +29,16 @@ exports.apply = (
       ])
     })
 
+    const {
+      iconPaths,
+      name,
+      themeColor,
+      assetsVersion,
+      msTileColor,
+      appleMobileWebAppCapable,
+      appleMobileWebAppStatusBarStyle
+    } = getAppConfig(appConfig)
+
     api.hooks.afterGenerate.tapPromise(ID, async () => {
       const { generateSW } = require('workbox-build')
       await generateSW({
@@ -39,7 +51,69 @@ exports.apply = (
         ].concat(generateSWOptions.globPatterns || [])
       })
 
-      await generateManifest(api, manifestOptions)
+      await generateManifest(api, {
+        iconPaths,
+        name,
+        themeColor
+      })
+    })
+
+    const { publicUrl } = api.config.build
+    const assetsVersionStr = assetsVersion ? `?v=${assetsVersion}` : ''
+
+    api.hooks.getDocumentData.tap(ID, data => {
+      data.meta += [
+        createElement('link', {
+          rel: 'manifest',
+          href: `${publicUrl}manifest.json${assetsVersionStr}`
+        }),
+        createElement('meta', {
+          name: 'theme-color',
+          content: themeColor
+        }),
+        // Add to home screen for Safari on iOS
+        iconPaths.appleTouchIcon &&
+          createElement('meta', {
+            name: 'apple-mobile-web-app-capable',
+            content: appleMobileWebAppCapable
+          }),
+        iconPaths.appleTouchIcon &&
+          createElement('meta', {
+            name: 'apple-mobile-web-app-status-bar-style',
+            content: appleMobileWebAppStatusBarStyle
+          }),
+        iconPaths.appleTouchIcon &&
+          createElement('meta', {
+            name: 'apple-mobile-web-app-title',
+            content: name
+          }),
+        iconPaths.appleTouchIcon &&
+          createElement('link', {
+            rel: 'apple-touch-icon',
+            href: `${publicUrl}${iconPaths.appleTouchIcon}${assetsVersionStr}`
+          }),
+        iconPaths.appleTouchIcon &&
+          createElement('link', {
+            rel: 'mask-icon',
+            href: `${publicUrl}${iconPaths.safariMaskIcon}${assetsVersionStr}`,
+            color: themeColor
+          }),
+
+        // Add to home screen for Windows
+        iconPaths.msTileImage &&
+          createElement('meta', {
+            name: 'msapplication-TileImage',
+            content: `${publicUrl}${iconPaths.msTileImage}${assetsVersionStr}`
+          }),
+        iconPaths.msTileImage &&
+          createElement('meta', {
+            name: 'msapplication-TileColor',
+            content: msTileColor
+          })
+      ]
+        .filter(Boolean)
+        .join('')
+      return data
     })
   }
 }
