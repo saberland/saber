992a6a354a6b549e43d3246718bee5bae424b4cb
EGOIST <0x142857@gmail.com>
2019-05-10 22:56:06 +0800

    feat: prettify output page component

--- packages/saber/vue-renderer/lib/page-prop-loader.js
+++ packages/saber/vue-renderer/lib/page-prop-loader.js
@@ -1,44 +1,47 @@
 const devalue = require('devalue')
+const prettier = require('prettier')
 const { requireAssets } = require('../../lib/utils/assetsAttribute')
 
 module.exports = function(source, map) {
   const pageId = source.trim()
   const { api } = this.query
   const page = requireAssets(devalue(api.pages.getPageProp(pageId)))
-  this.callback(
-    null,
-    `
-    export default function(Component) {
-      var page = ${page}
-      var beforeCreate = Component.options.beforeCreate || []
-      Component.options.beforeCreate = [function() {
-        this.$page = page
-      }].concat(beforeCreate)
+  let result = `
+  export default function(Component) {
+    var page = ${page}
+    var beforeCreate = Component.options.beforeCreate || []
+    Component.options.beforeCreate = [function() {
+      this.$page = page
+    }].concat(beforeCreate)
 
-      // These options can be defined as Vue component option or page attribute
-      // They are also available in layout component except for the 'layout' option
-      var pageComponentOptions = ['layout', 'transition']
+    // These options can be defined as Vue component option or page attribute
+    // They are also available in layout component except for the 'layout' option
+    var pageComponentOptions = ['layout', 'transition']
 
-      pageComponentOptions.forEach(function(name) {
-        var PageComponent = Component.options.PageComponent
-        if (PageComponent) {
-          // .vue or .js page, set route transition from PageComponent
-          Component.options[name] = PageComponent[name]
-        }
-
-        // Fallback to page attribute
-        if (Component.options[name] === undefined) {
-          Component.options[name] = page.attributes[name]
-        }
-      })
-
-      Component.options.name = 'page-wrapper-' + page.attributes.slug.replace(/[^0-9a-z\\-]/i, '-')
-      if (module.hot) {
-        var Vue = require('vue').default
-        Component.options._Ctor = Vue.extend(Component)
+    pageComponentOptions.forEach(function(name) {
+      var PageComponent = Component.options.PageComponent
+      if (PageComponent) {
+        // .vue or .js page, set route transition from PageComponent
+        Component.options[name] = PageComponent[name]
       }
+
+      // Fallback to page attribute
+      if (Component.options[name] === undefined) {
+        Component.options[name] = page.attributes[name]
+      }
+    })
+
+    Component.options.name = 'page-wrapper-' + page.attributes.slug.replace(/[^0-9a-z\\-]/i, '-')
+    if (module.hot) {
+      var Vue = require('vue').default
+      Component.options._Ctor = Vue.extend(Component)
+    }
   }
-  `,
-    map
-  )
+  `
+
+  if (process.env.NODE_ENV !== 'production') {
+    result = prettier.format(result, { parser: 'babel' })
+  }
+
+  this.callback(null, result, map)
 }
