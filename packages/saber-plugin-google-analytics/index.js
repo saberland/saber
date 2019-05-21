58a421fcce11b5b2254af65cb8d18dd010e30d12
EGOIST <0x142857@gmail.com>
2019-05-21 15:35:48 +0800

    no need to add respectDoNotTrack option

--- packages/saber-plugin-google-analytics/index.js
+++ packages/saber-plugin-google-analytics/index.js
@@ -4,12 +4,11 @@ const ID = 'google-analytics'
 
 exports.name = ID
 
-exports.apply = (api, { trackId = false, respectDoNotTrack = true } = {}) => {
+exports.apply = (api, { trackId = false } = {}) => {
   api.hooks.chainWebpack.tap(ID, config => {
     config.plugin('constants').tap(([options]) => [
       Object.assign(options, {
-        __GA_TRACK_ID__: JSON.stringify(trackId),
-        respectDoNotTrack
+        __GA_TRACK_ID__: JSON.stringify(trackId)
       })
     ])
   })
--- packages/saber-plugin-google-analytics/saber-browser.js
+++ packages/saber-plugin-google-analytics/saber-browser.js
@@ -1,11 +1,29 @@
 /* eslint-disable */
 // Google analytics integration for Vue.js renderer
-export default function (ctx) {
-  var router = ctx.router
-  if (process.browser && process.env.NODE_ENV === 'production' && __GA_TRACK_ID__ &&
-    (!respectDoNotTrack || !(navigator.msDoNotTrack || window.doNotTrack || navigator.doNotTrack))
+export default function({ router }) {
+  if (
+    process.browser &&
+    process.env.NODE_ENV === 'production' &&
+    __GA_TRACK_ID__
   ) {
-    (function (i, s, o, g, r, a, m) {
+    function doNotTrackEnabled() {
+      const dntNumber = parseInt(
+        navigator.msDoNotTrack || // Internet Explorer 9 and 10 vendor prefix
+        window.doNotTrack || // IE 11 uses window.doNotTrack
+          navigator.doNotTrack, // W3C
+        10
+      )
+
+      return dntNumber === 1
+    }
+
+    if (doNotTrackEnabled()) {
+      // Respect doNotTrack setting
+      return
+    }
+
+    // prettier-ignore
+    ;(function(i, s, o, g, r, a, m) {
       i.GoogleAnalyticsObject = r
       i[r] = i[r] || function () {
         (i[r].q = i[r].q || []).push(arguments)
@@ -21,7 +39,7 @@ export default function (ctx) {
     ga('create', __GA_TRACK_ID__, 'auto')
     ga('send', 'pageview')
 
-    router.afterEach(function (to) {
+    router.afterEach(to => {
       ga('set', 'page', to.fullPath)
       ga('send', 'pageview')
     })
