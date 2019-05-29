2286ecdcb556db47eaf1e6267e20b5ba6601a995
Max <hi@krmax44.de>
2019-05-29 16:04:43 +0200

    fix: Scroll to anchor not working #213 (#214)
    closes #213 
    
    * fix: #213
    
    * add scroll on initial load
    
    * fix: initial page load scroll on Chromium
    
    * remove css.escape
    
    * remove unnecessary code
    
    * avoid selector escaping issues

--- packages/saber/vue-renderer/app/router.js
+++ packages/saber/vue-renderer/app/router.js
@@ -31,65 +31,57 @@ if (process.client) {
 }
 
 export default () => {
-  const createRouter = routes => new Router({
-    mode: 'history',
-    routes,
-    scrollBehavior(to, from, savedPosition) {
-      // if the returned position is falsy or an empty object,
-      // will retain current scroll position.
-      let position = false
+  const createRouter = routes =>
+    new Router({
+      mode: 'history',
+      routes,
+      scrollBehavior(to, from, savedPosition) {
+        // if the returned position is falsy or an empty object,
+        // will retain current scroll position.
+        let position = false
 
-      // if no children detected and scrollToTop is not explicitly disabled
-      if (
-        to.matched.length < 2 &&
-        to.matched.every(
-          r => r.components.default.scrollToTop !== false
-        )
-      ) {
-        // scroll to the top of the page
-        position = { x: 0, y: 0 }
-      } else if (
-        to.matched.some(r => r.components.default.scrollToTop)
-      ) {
-        // if one of the children has scrollToTop option set to true
-        position = { x: 0, y: 0 }
-      }
+        // if no children detected and scrollToTop is not explicitly disabled
+        if (
+          to.matched.length < 2 &&
+          to.matched.every(r => r.components.default.scrollToTop !== false)
+        ) {
+          // scroll to the top of the page
+          position = { x: 0, y: 0 }
+        } else if (to.matched.some(r => r.components.default.scrollToTop)) {
+          // if one of the children has scrollToTop option set to true
+          position = { x: 0, y: 0 }
+        }
 
-      // savedPosition is only available for popstate navigations (back button)
-      if (savedPosition) {
-        position = savedPosition
-      }
+        // savedPosition is only available for popstate navigations (back button)
+        if (savedPosition) {
+          position = savedPosition
+        }
 
-      return new Promise(resolve => {
-        // wait for the out transition to complete (if necessary)
-        router.app.$once('trigger-scroll', () => {
-          // coords will be used if no selector is provided,
-          // or if the selector didn't match any element.
-          if (to.hash) {
-            let hash = to.hash
-            // CSS.escape() is not supported with IE and Edge.
-            if (
-              typeof window.CSS !== 'undefined' &&
-              typeof window.CSS.escape !== 'undefined'
-            ) {
-              hash = '#' + window.CSS.escape(hash.substr(1))
-            }
-            try {
-              if (document.querySelector(hash)) {
+        return new Promise(resolve => {
+          const fulfill = () => {
+            // coords will be used if no selector is provided,
+            // or if the selector didn't match any element.
+            if (to.hash) {
+              if (document.getElementById(to.hash.substr(1))) {
                 // scroll to anchor by returning the selector
-                position = { selector: hash }
+                position = { selector: to.hash }
+              } else {
+                // scroll to top if anchor does not exist and position is not already set
+                position = position || { x: 0, y: 0 }
               }
-            } catch (e) {
-              console.warn(
-                'Failed to save scroll position. Please add CSS.escape() polyfill (https://github.com/mathiasbynens/CSS.escape).'
-              )
             }
+            resolve(position)
           }
-          resolve(position)
+
+          // wait for the out transition to complete (if necessary)
+          if (to.path === from.path) {
+            fulfill()
+          } else {
+            router.app.$once('trigger-scroll', fulfill)
+          }
         })
-      })
-    }
-  })
+      }
+    })
 
   const router = createRouter(routes)
 
