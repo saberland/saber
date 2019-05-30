3104e25b5bc3e31f9dc0d77186c160cdf399eae0
EdelWolle <i-bn@live.fr>
2019-05-30 02:00:25 +0000

    Use AMP optimizer and support hybrid pages

--- packages/saber-plugin-amp/lib/index.js
+++ packages/saber-plugin-amp/lib/index.js
@@ -12,17 +12,56 @@ exports.apply = api => {
       const outputDir = api.resolveCache('public')
 
       context.documentData = api.getDocumentData(context)
+
       context.documentData.htmlAttrs += ' amp'
-      context.documentData.link += `<link rel="canonical" href="${
-        context.url
-      }">`
-      context.documentData.style =
-        context.documentData.style.replace('<style', '<style amp-custom') +
-        `<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style>`
-      context.documentData.noscript += `<noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>`
-      context.documentData.headScript += `<script async src="https://cdn.ampproject.org/v0.js"></script>`
-      // amp-script is experimental for now
-      // this should change when it becomes available for production
+      context.documentData.style = context.documentData.style.replace(
+        '<style',
+        '<style amp-custom'
+      )
+
+      const ampStyle = `<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style>`
+      const ampNoScript = `<noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>`
+      const ampScript = `<script async src="https://cdn.ampproject.org/v0.js"></script>`
+      const canonicalLink = `<link rel="canonical" href="${context.url}">`
+
+      context.documentData.link += canonicalLink
+      // Using fuzzball may be a better choice minified 63KB take 0.5s to download with 3G
+      if (context.documentData.style.indexOf(ampStyle) === -1) {
+        context.documentData.style += ampStyle
+      }
+
+      if (context.documentData.noscript.indexOf(ampNoScript) === -1) {
+        context.documentData.noscript += ampNoScript
+      }
+
+      if (context.documentData.headScript.indexOf(ampScript) === -1) {
+        context.documentData.headScript += ampScript
+      }
+
+      const AmpOptimizer = require('amp-toolbox-optimizer')
+      const optimizer = AmpOptimizer.create() // no additional config required for valid AMP mode
+
+      if (context.amp === 'hybrid') {
+        // output Hybrid page
+        const htmlWithScript = `<!DOCTYPE html>${api.getDocument(
+          context.documentData
+        )}`
+          .replace(/^\s+/gm, '')
+          .replace(/\n+</g, '<')
+          .replace('<div id="_saber"></div>', exportedPage.markup)
+
+        const hybridTransformedHtml = await optimizer.transformHtml(
+          htmlWithScript
+        )
+
+        const outputFilePath = exportedPage.path
+        log.info(
+          'Generating hybrid AMP page',
+          path.relative(outputDir, outputFilePath)
+        )
+        await fs.outputFile(outputFilePath, hybridTransformedHtml, 'utf8')
+      }
+
       context.documentData.bodyScript = ``
 
       const html = `<!DOCTYPE html>${api.getDocument(context.documentData)}`
@@ -30,9 +69,17 @@ exports.apply = api => {
         .replace(/\n+</g, '<')
         .replace('<div id="_saber"></div>', exportedPage.markup)
 
-      const outputFilePath = exportedPage.path.replace('.html', '.amp.html')
-      log.info('Generating', path.relative(outputDir, outputFilePath))
-      await fs.outputFile(outputFilePath, html, 'utf8')
+      const transformedHtml = await optimizer.transformHtml(html)
+
+      const outputFilePath = exportedPage.path.replace(
+        '.html',
+        '.amp/index.html'
+      )
+      log.info(
+        'Generating valid AMP page',
+        path.relative(outputDir, outputFilePath)
+      )
+      await fs.outputFile(outputFilePath, transformedHtml, 'utf8')
     }
   })
 }
