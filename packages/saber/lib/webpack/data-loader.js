2f7d4d0389b932e44a04843c1f398c8863919a1c
EdelWolle <i-bn@live.fr>
2019-06-09 11:45:48 +0100

    tweaks

--- packages/saber/lib/webpack/data-loader.js
+++ packages/saber/lib/webpack/data-loader.js
@@ -1,14 +1,16 @@
 const vm = require('vm')
 const devalue = require('devalue')
+const loaderUtils = require('loader-utils')
 
 module.exports = async function(source) {
   const callback = this.async()
+  const params = loaderUtils.parseQuery(this.resourceQuery)
   const sandbox = {
     require,
     module: {}
   }
   vm.createContext(sandbox)
   const mod = vm.runInContext(source, sandbox)
-  const result = await mod(this.resourceQuery.replace('?name=', ''))
+  const result = await mod(params.name)
   return callback(null, `export default ${devalue(result)}`)
 }
