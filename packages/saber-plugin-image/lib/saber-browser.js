22327d2210eedad95eebe8373944e9fbaae3060d
Kid <kidonng@gmail.com>
2019-07-13 03:08:00 +0800

    fix: fix `srcset` lazy load

--- packages/saber-plugin-image/lib/saber-browser.js
+++ packages/saber-plugin-image/lib/saber-browser.js
@@ -3,7 +3,7 @@ import styles from './styles.module.css'
 
 export default ({ Vue }) => {
   const options = Object.assign(
-    (process.browser && __SABER_IMAGE_OPTIONS__) || {}, // eslint-disable-line no-undef
+    __SABER_IMAGE_OPTIONS__ || {}, // eslint-disable-line no-undef
     { lazyComponent: true }
   )
 
@@ -40,7 +40,7 @@ export default ({ Vue }) => {
           })
         }
 
-        const { width, height, src, srcSet: srcset, placeholder } = this.src
+        const { width, height, src, srcSet, placeholder } = this.src
 
         const loading =
           (getOption('placeholder') && placeholder) ||
@@ -52,7 +52,7 @@ export default ({ Vue }) => {
         return h('img', {
           attrs: {
             ...$attrs,
-            srcset: options.sizes !== false && srcset,
+            'data-srcset': srcSet,
             width,
             height
           },
@@ -84,7 +84,7 @@ export default ({ Vue }) => {
 
       const { src, srcSet: srcset } = this.src
       return h('img', {
-        attrs: { ...$attrs, src, srcset: options.sizes !== false && srcset }
+        attrs: { ...$attrs, src, srcset }
       })
     }
   })
