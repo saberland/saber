ef332567486015e65fdd8de21756d8c0cab46e3b
EGOIST <0x142857@gmail.com>
2019-09-07 20:47:12 +0800

    optimize the logo

--- website/src/components/Logo.vue
+++ website/src/components/Logo.vue
@@ -1,65 +1,36 @@
 <template>
-  <svg
-    :width="size"
-    :height="size"
-    viewBox="0 0 402 402"
-    version="1.1"
-    xmlns="http://www.w3.org/2000/svg"
-    xmlns:xlink="http://www.w3.org/1999/xlink"
-  >
-    <!-- Generator: Sketch 56.2 (81672) - https://sketch.com -->
-    <title>编组 6</title>
-    <desc>Created with Sketch.</desc>
-    <g id="页面1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-      <g id="编组-6" transform="translate(1.000000, 1.000000)">
-        <g id="编组-4">
-          <circle id="椭圆形" stroke="#00838F" fill="#00838F" cx="200" cy="200" r="200.5" />
-          <path
-            d="M152.842359,232.240687 L203.5,228 L213.023656,250 L180.330798,282.43453 L152.842359,321.07787 L156.470397,368 C129.583323,361.434315 111.677654,354.963171 102.753391,348.586566 C93.8291281,342.209961 84.2446643,333.040396 74,321.07787 L122,263.912735 L143,263.912735 L152.842359,232.240687 Z"
-            id="矩形"
-            fill="#FBB526"
-          />
-          <rect id="矩形" fill="#FFFFFF" x="143" y="131" width="120" height="14" />
-          <rect id="矩形" fill="#FFFFFF" x="171" y="145" width="12" height="83" />
-          <rect id="矩形" fill="#FFFFFF" x="224" y="145" width="12" height="105" />
-          <rect id="矩形" fill="#FFFFFF" x="196" y="185" width="12" height="40" />
-          <rect id="矩形" fill="#FFFFFF" x="187" y="91" width="33" height="40" />
-          <circle id="椭圆形" stroke="#FFFFFF" stroke-width="14" cx="203" cy="78" r="18" />
-          <path d id="路径-2" stroke="#979797" />
-          <circle id="椭圆形" stroke="#FFFFFF" stroke-width="14" cx="200" cy="200" r="179" />
-          <polyline
-            id="路径"
-            stroke="#FFFFFF"
-            stroke-width="14"
-            points="70 325.804841 119.367626 260 147.867626 260"
-          />
-          <polyline
-            id="路径-3"
-            stroke="#FFFFFF"
-            stroke-width="14"
-            points="122 314.068663 144.74769 294.337742 150.247957 236.435321 205.24769 226 220 250"
-          />
-          <polyline
-            id="路径-4"
-            stroke="#FFFFFF"
-            stroke-width="14"
-            points="177 286.057533 196 274.409932 205.187862 250 250.37934 254.615314 267.647602 277.436818"
-          />
-          <polyline
-            id="路径-5"
-            stroke="#FFFFFF"
-            stroke-width="14"
-            points="242.721241 274.409932 279.618156 274.409932 330.993155 324"
-          />
-          <polyline
-            id="路径-6"
-            stroke="#FFFFFF"
-            stroke-width="14"
-            points="200 331.155739 221.037121 311 240 320.931337 280.629583 326.440549"
-          />
-        </g>
-        <g id="编组" transform="translate(74.000000, 53.000000)" />
-      </g>
+  <svg xmlns="http://www.w3.org/2000/svg" :width="size" :height="size" viewBox="0 0 402 402">
+    <g fill="none" fill-rule="evenodd" transform="translate(1 1)">
+      <circle cx="200" cy="200" r="200.5" fill="#00838F" stroke="#00838F" />
+      <path
+        fill="#FBB526"
+        d="M152.842359,232.240687 L203.5,228 L213.023656,250 L180.330798,282.43453 L152.842359,321.07787 L156.470397,368 C129.583323,361.434315 111.677654,354.963171 102.753391,348.586566 C93.8291281,342.209961 84.2446643,333.040396 74,321.07787 L122,263.912735 L143,263.912735 L152.842359,232.240687 Z"
+      />
+      <rect width="120" height="14" x="143" y="131" fill="#FFF" />
+      <rect width="12" height="83" x="171" y="145" fill="#FFF" />
+      <rect width="12" height="105" x="224" y="145" fill="#FFF" />
+      <rect width="12" height="40" x="196" y="185" fill="#FFF" />
+      <rect width="33" height="40" x="187" y="91" fill="#FFF" />
+      <circle cx="203" cy="78" r="18" stroke="#FFF" stroke-width="14" />
+      <path stroke="#979797" />
+      <circle cx="200" cy="200" r="179" stroke="#FFF" stroke-width="14" />
+      <polyline stroke="#FFF" stroke-width="14" points="70 325.805 119.368 260 147.868 260" />
+      <polyline
+        stroke="#FFF"
+        stroke-width="14"
+        points="122 314.069 144.748 294.338 150.248 236.435 205.248 226 220 250"
+      />
+      <polyline
+        stroke="#FFF"
+        stroke-width="14"
+        points="177 286.058 196 274.41 205.188 250 250.379 254.615 267.648 277.437"
+      />
+      <polyline stroke="#FFF" stroke-width="14" points="242.721 274.41 279.618 274.41 330.993 324" />
+      <polyline
+        stroke="#FFF"
+        stroke-width="14"
+        points="200 331.156 221.037 311 240 320.931 280.63 326.441"
+      />
     </g>
   </svg>
 </template>
