9a885512ab5d33c15d64b5ad9c6dc7d617f3eb57
EGOIST <0x142857@gmail.com>
2019-09-25 20:51:16 +0800

    chore: add example for vue-composition-api

---
+++ examples/vue-composition-api/README.md
@@ -0,0 +1,5 @@
+# vue-composition-api
+
+With `@vue/composition-api` you can enjoy the upcoming Vue 3.x feature in Vue 2.x right away.
+
+[![Edit saber-sandbox](https://codesandbox.io/static/img/play-codesandbox.svg)](https://codesandbox.io/s/github/saberland/saber/tree/master/examples/vue-composition-api)
---
+++ examples/vue-composition-api/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <button @click="increment">
+    {{ state.count }} * 2 = {{ doubleCount }}
+  </button>
+</template>
+
+<script>
+// Using Vue 3.x composition API in Vue 2.x
+import { createComponent, reactive, computed } from '@vue/composition-api'
+
+// Wrapping component definition with `createComponent`
+// is only required for TypeScript users
+export default createComponent({
+  setup() {
+    const state = reactive({
+      count: 0
+    })
+    const doubleCount = computed(() => state.count * 2)
+    return {
+      state,
+      doubleCount,
+      increment() {
+        state.count++
+      }
+    }
+  }
+})
+</script>
