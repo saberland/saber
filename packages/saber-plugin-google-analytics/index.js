29989a291301b258d784b4b936489f90d3a56032
andreas <andreasvirkus@gmail.com>
2019-10-06 18:56:56 +0300

    Respect browser's Do Not Track setting

--- packages/saber-plugin-google-analytics/index.js
+++ packages/saber-plugin-google-analytics/index.js
@@ -2,9 +2,25 @@ const path = require('path')
 
 const ID = 'google-analytics'
 
+const allowsTracking = () => {
+  const dnt = window.doNotTrack ||
+    navigator.doNotTrack ||
+    navigator.msDoNotTrack
+
+  if (dnt === 1 || dnt === '1' || dnt === 'yes') return false
+
+  if ('msTrackingProtectionEnabled' in window.external) {
+    return !window.external.msTrackingProtectionEnabled()
+  }
+
+  return true
+}
+
 exports.name = ID
 
 exports.apply = (api, { trackId = false, anonymizeIp = false } = {}) => {
+  if (!allowsTracking()) return
+
   api.hooks.chainWebpack.tap(ID, config => {
     config.plugin('constants').tap(([options]) => [
       Object.assign(options, {
