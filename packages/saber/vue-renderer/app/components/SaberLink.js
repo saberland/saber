8e3daa9e4621a33013118908203a1db22723065b
EGOIST <0x142857@gmail.com>
2019-10-07 05:18:18 +0800

    fix: use `<a>` if the page doesn't exist
    fix #506

--- packages/saber/vue-renderer/app/components/SaberLink.js
+++ packages/saber/vue-renderer/app/components/SaberLink.js
@@ -16,6 +16,7 @@ export default {
   render(h, { data, children, parent }) {
     const attrs = { ...data.attrs }
     const isExternal = typeof attrs.to === 'string' && isAbsoluteUrl(attrs.to)
+    let tag = 'a'
 
     if (isExternal) {
       if (HTTP_RE.test(attrs.to)) {
@@ -29,8 +30,16 @@ export default {
       delete attrs.to
     } else {
       if (typeof attrs.to === 'string') {
-        attrs.to = parent.$saber.getPageLink(attrs.to)
+        const link = parent.$saber.getPageLink(attrs.to)
+        if (link) {
+          tag = 'router-link'
+          attrs.to = link
+        } else {
+          attrs.href = attrs.to
+          delete attrs.to
+        }
       } else {
+        tag = 'router-link'
         const { route } = parent.$router.resolve(attrs.to)
         attrs.to = parent.$saber.getPageLink(route.fullPath)
       }
@@ -39,7 +48,7 @@ export default {
     delete attrs.openLinkInNewTab
 
     return h(
-      isExternal ? 'a' : 'router-link',
+      tag,
       {
         ...data,
         attrs
