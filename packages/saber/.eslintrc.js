4570761a5ffcc0e940d7935b3498fe1aed6d31d1
EGOIST <0x142857@gmail.com>
2020-07-04 01:11:46 +0800

    disable eslint rule ban-ts-ignore

--- packages/saber/.eslintrc.js
+++ packages/saber/.eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/triple-slash-reference': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off'
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'off'
   }
 }
